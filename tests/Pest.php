eebdab1f7b6b92b76a59ccd84cf20970f4d05334
Modelesque <ryan@modelesque.dev>
2026-01-21 19:23:12 +0100

    feat: improving Pest's entry point file to refresh the db after feature tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modelesque\Api\Spotify\Tests\TestCase;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class, RefreshDatabase::class)->in('Feature');
+uses(TestCase::class)->in('Unit');
+
+// clean up Mockery after each test
+afterEach(fn() => Mockery::close());
